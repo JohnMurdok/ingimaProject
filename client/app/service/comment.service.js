1addb221364f57c6b96108440831aaaebcc5e410
JohnMurdok <romain.carroussel@viacesi.fr>
2016-12-10 20:34:26 +0100

    Move to /server

--- client/app/service/comment.service.js
+++ client/app/service/comment.service.js
@@ -47,9 +47,10 @@ var CommentService = (function () {
     };
     CommentService = __decorate([
         core_1.Injectable(), 
-        __metadata('design:paramtypes', [http_1.Http])
+        __metadata('design:paramtypes', [(typeof (_a = typeof http_1.Http !== 'undefined' && http_1.Http) === 'function' && _a) || Object])
     ], CommentService);
     return CommentService;
+    var _a;
 }());
 exports.CommentService = CommentService;
 //# sourceMappingURL=comment.service.js.map
--- client/app/service/post.service.js
+++ client/app/service/post.service.js
@@ -46,9 +46,10 @@ var PostService = (function () {
     };
     PostService = __decorate([
         core_1.Injectable(), 
-        __metadata('design:paramtypes', [http_1.Http])
+        __metadata('design:paramtypes', [(typeof (_a = typeof http_1.Http !== 'undefined' && http_1.Http) === 'function' && _a) || Object])
     ], PostService);
     return PostService;
+    var _a;
 }());
 exports.PostService = PostService;
 //# sourceMappingURL=post.service.js.map
